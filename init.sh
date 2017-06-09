eb0a485a3543311ca7ac5e0dedf331bdf683ecf7
Patrick Fischer <pat.fischer@tum.de>
2017-06-09 09:21:21 +0200

    Initial Commit

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#devDependencies for webpack
+npm i --save babel-core webpack webpack-dev-server bignumber.js babel-loader babel-preset-es2015
+npm i style-loader css-loader
+npm i copy-webpack-plugin
