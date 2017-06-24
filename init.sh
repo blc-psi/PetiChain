7986091f21b714e470df90bb8e2bbeb0b4976ea8
Patrick Fischer <pat.fischer@tum.de>
2017-06-24 20:26:00 +0200

    navbar added

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #devDependencies for webpack
+npm i web3
 npm i --save babel-core webpack webpack-dev-server bignumber.js babel-loader babel-preset-es2015
 npm i style-loader css-loader
 npm i copy-webpack-plugin
